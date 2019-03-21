aa12b9d456532bb24792231471af151fd92b8a43
Tinuade Adeleke <summittinuade@gmail.com>
2019-03-21 02:43:17 +0100

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
